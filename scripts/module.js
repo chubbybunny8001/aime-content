f66a10be3292236a4ee4b500e2d346262f71f8a8
Josiah Campbell Sanchez <josiahbear223@gmail.com>
2024-05-09 16:43:00 -0600

    getting scripts ready to update links

--- scripts/module.js
+++ scripts/module.js
@@ -1,1 +1,13 @@
 console.log("aime-content | Hello World");
+
+const updateCompenidaLinks = async function () {
+  const keyBackgrounds = "aime-content.backgroundssrd"
+  const backgroundFeature = await game.packs.get(keyBackgrounds).getDocuments({ type: "background" });
+  
+  console.log(backgroundFeature)
+}
+
+// After Game Load
+Hooks.on("ready", function () {
+  updateCompenidaLinks()
+});
